1a392563b838cb47683193761b999b3db7bc8cc0
Anton Bykov <nesh90210@gmail.com>
2020-06-11 17:14:57 +0600

    Добавил RecyclerView в MainActivity и создал MainListAdapter для RecyclerView в Main

--- app/src/main/java/ru/antoxeeen/buynow/view/MainActivity.java
+++ app/src/main/java/ru/antoxeeen/buynow/view/MainActivity.java
@@ -3,6 +3,8 @@ package ru.antoxeeen.buynow.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import ru.antoxeeen.buynow.R;
 import ru.antoxeeen.buynow.repository.MainList;
 import ru.antoxeeen.buynow.viewmodel.MainListsViewModel;
@@ -20,13 +22,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.main_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final MainListAdapter adapter = new MainListAdapter();
+        recyclerView.setAdapter(adapter);
+
         viewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication())
                 .create(MainListsViewModel.class);
         viewModel.getAllMainLists().observe(this, new Observer<List<MainList>>() {
             @Override
             public void onChanged(List<MainList> mainLists) {
-                //update recyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setList(mainLists);
             }
         });
     }
---
+++ app/src/main/java/ru/antoxeeen/buynow/view/MainListAdapter.java
@@ -0,0 +1,52 @@
+package ru.antoxeeen.buynow.view;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import ru.antoxeeen.buynow.R;
+import ru.antoxeeen.buynow.repository.MainList;
+
+class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.MainListHolder> {
+    private List<MainList> mainList = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public MainListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+        return new  MainListHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainListHolder holder, int position) {
+        MainList currentMainList = mainList.get(position);
+        holder.textViewTitle.setText(currentMainList.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mainList.size();
+    }
+
+    public void setList(List<MainList> mainList){
+        this.mainList = mainList;
+        notifyDataSetChanged();
+    }
+
+    class MainListHolder extends RecyclerView.ViewHolder{
+        private TextView textViewTitle;
+
+        public MainListHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_list);
+        }
+    }
+}
