a97dde3f9bb7c4fdef2c05b2d88ad02d16c3c1fc
Anton Bykov <nesh90210@gmail.com>
2020-06-13 12:51:08 +0600

    Добавил активити activity_add_list.xml, добавил иконки, для сохранения, закрытия и добавления. Добавил FloatingButton на mainActivity для добавления нового листа.

---
+++ app/src/main/java/ru/antoxeeen/buynow/view/AddListActivity.java
@@ -0,0 +1,15 @@
+package ru.antoxeeen.buynow.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ru.antoxeeen.buynow.R;
+
+import android.os.Bundle;
+
+public class AddListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_list);
+    }
+}
